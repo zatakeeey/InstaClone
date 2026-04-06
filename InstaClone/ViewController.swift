af56f35f9042b8d622680a779176318ef360636a
Polezhaev Georgiy <zatakey222@gmail.com>
2026-04-06 15:07:28 +0300

    Initial Commit

---
+++ InstaClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaClone
+//
+//  Created by Георгий Полежаев on 06.04.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
